032e19a93b902241f614ceab14edac305b81b71b
Hamza <114071457+Hamza2702@users.noreply.github.com>
2024-08-31 23:07:02 +0100

    Update ReverseListGenerics.java

--- random/ReverseListGenerics.java
+++ random/ReverseListGenerics.java
@@ -1,3 +1,12 @@
+public static void main(String[] args) {
+		
+		List<Integer> l = new ArrayList<>();
+		l.add(1); l.add(2); l.add(3); l.add(4);
+		System.out.println(l);
+		reverse(l);
+		System.out.println(l);
+	}
+
 public static <T> void reverse(List<T> l){
 		final int lastPosition = l.size() - 1;
 		for (int i = 0; i < l.size() / 2; i++){
